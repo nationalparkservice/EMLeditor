391abcee735b310ded0a4ba7966329a1bf60351e
Robert L. Baker <robert_baker@nps.gov>
2022-09-27 15:05:36 -0600

    remove alaised functions during debug

--- R/utils.R
+++ R/utils.R
@@ -1,26 +1,3 @@
-#' alias for eml_get_simple
-#'
-#' @decription eml_get_simpleNPS is an alias for arcticdatautils eml_get_simple function
-#'
-#' @details eml_get_simpleNPS serves as an alias for the arcticdatautils function eml_get_simple. This means the eml_get_simple() function will be called using current local version of arcticdatautils on the users's machine rather than whatever version was available when the package was built.
-#'
-#' @param emlObject is an R object imported (typically from an EML-formatted .xml file) using EmL::read_eml(<filename>, from="xml").
-#' @param elment is the name of the element to be extracted. If multiple occurances are found, will extract all.
-#' @export
-eml_get_simpleNPS<-function(...)arcticdatautils::eml_get_simple()
-
-#' alias for eml_get
-#'
-#' @decription eml_getNPS is an alias for the EML get_simple function
-#'
-#' @details eml_getNPS serves as an alias for the EML function eml_get. This means the eml_get() function will be called using current local version of EML on the users's machine rather than whatever version was available when the package was built.
-#'
-#' @param emlObject is an R object imported (typically from an EML-formatted .xml file) using EmL::read_eml(<filename>, from="xml").
-#' @param elment is the name of the element to be extracted. If multiple occurrences are found, will extract all.
-#' @export
-eml_getNPS<-function(...)EML::eml_get()
-
-
 #' inject NPS info into metadata
 #'
 #' @description injects static NPS-specific info into eml documents
@@ -29,17 +6,18 @@ eml_getNPS<-function(...)EML::eml_get()
 #'
 #' @param emlObject is an R object imported (typically from an EML-formatted .xml file) using EmL::read_eml(<filename>, from="xml").
 #'
-#' @return
+#' @return emlObject
 #' @export
 #'
 #' @examples
+#' set.NPSpublisher(emlObject)
 set.NPSpublisher<-function(emlObject){
   #get existing publisher info for the data package:
   publish<-emlObject$dataset$publisher
 
   #create desired publisher info:
   pubset<- list(organizationName =
-            "U.S. National Park Service",
+            "National Park Service",
           address = list(deliveryPoint = "1201 Oakridge Drive, Suite 150",
                          city = "Fort Collins",
                          administrativeArea="CO",
@@ -61,9 +39,10 @@ set.NPSpublisher<-function(emlObject){
 #' @description set.version adds the current version of EMLeditor to the EML document.
 #'
 #' @details set.version adds the current version of EMLeditor to the metadata, specifically in the "additionalMetadata" element
-#' @param emlObject
 #'
-#' @return
+#' @param emlObject is an R object imported (typically from an EML-formatted .xml file) using EML::read_eml(<filename>, from="xml").
+#'
+#' @return emlObject
 #' @export
 #'
 #' @examples
