2525ba9addf40061a2eba3af2ea07a99e1f69c1b
Rob Baker <robert_baker@nps.gov>
2023-10-06 14:30:26 -0600

    update to allow irmadev interactions

--- R/utils.R
+++ R/utils.R
@@ -9,12 +9,19 @@ assign("ds_api", "https://irmaservices.nps.gov/datastore/v6/rest/", envir=.pkggl
 #data_store secure API base URL:
 assign("ds_secure_api", "https://irmaservices.nps.gov/datastore-secure/v6/rest/", envir=.pkgglobalenv)
 
+#data_store dev api (requires secure)
+assign("ds_dev_api", "https://irmadevservices.nps.gov/datastore/v6/rest/", envir = .pkgglobalenv)
+
 .ds_api <- function(x){
   get("ds_api", envir = .pkgglobalenv)
 }
 
 .ds_secure_api <- function(x){
   get("ds_secure_api", envir = .pkgglobalenv)
+}
+
+.ds_dev_api <- function(x){
+  get("ds_dev_api", envir = .pkgglobalenv)
 }
 
 #this gets rid of the "no visible binding for global variable 'x'" error in build checks:
