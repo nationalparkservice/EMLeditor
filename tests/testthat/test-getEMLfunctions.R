0aee824a4fbd30892b5233dfc18c78219d6a62d0
mdkompella <mkompella143@gmail.com>
2025-04-18 15:42:16 -0600

    add ignore.case for csv ending in list.files

--- tests/testthat/test-getEMLfunctions.R
+++ tests/testthat/test-getEMLfunctions.R
@@ -193,7 +193,9 @@ test_that("object input returns list output", {
 # test length of nested attribute table, should be equal to the number of csv files in the data package
 test_that("output table length equals number of csvs in data package", {
   expect_equal(length(get_attribute_tables(BICY_EMLed_meta)), length(list.files(testthat::test_path("good",
-                                                                                                    "BICY"), pattern = "csv")))
+                                                                                                    "BICY"),
+                                                                                pattern = "csv",
+                                                                                ignore.case = TRUE)))
 })
 
 # test bad eml object
